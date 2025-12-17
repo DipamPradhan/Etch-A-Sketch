13d6fc74e26674a883d19317770b1c8a66550ebe
Dipam Pradhan <meedipam@gmail.com>
2025-12-17 23:22:20 +0545

    add a dedicated draw and clear button

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const container = document.getElementById("container");
-const button = document.getElementById("createGrid");
 const userInput = document.getElementById("size");
+const createGrid = document.getElementById("createGrid")
+const eraseGrid = document.getElementById("eraseGrid")
+const inputDiv = document.querySelector(".input")
 
 let GRID_SIZE = 16;
+
+function createButton(id, text) {
+  const btn = document.createElement("button");
+  btn.id = id;
+  btn.textContent = text;
+  return btn;
+}
+
 
 function getRandomColor() {
   const r = Math.floor(Math.random() * 256);
@@ -11,7 +21,10 @@ function getRandomColor() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-button.addEventListener("click", () => {
+
+let drawBtn, clearBtn;
+let isDrawing = false;
+createGrid.addEventListener("click", () => {
   const val = Number(userInput.value);
 
   if (val < 1 || val > 100) {
@@ -24,10 +37,28 @@ button.addEventListener("click", () => {
   // clear old grid
   container.innerHTML = "";
   container.style.border = "2px solid black";
+  drawBtn?.remove()
+  clearBtn?.remove()
+
+  drawBtn = createButton("drawGrid", "Draw");
+  clearBtn = createButton("clearGrid", "Clear");
   // resize container
   container.style.width = `${GRID_SIZE * 30}px`;
   container.style.height = `${GRID_SIZE * 30}px`;
+  inputDiv.append(drawBtn, clearBtn);
 
+  clearBtn.addEventListener("click",()=>{
+    isDrawing = false
+      const squares = container.querySelectorAll(".square");
+
+      squares.forEach(square => {
+        square.style.backgroundColor = "";
+      });
+    })
+
+  drawBtn.addEventListener("click",()=>{
+    isDrawing = true
+  })
   const TOTAL_SQUARES = GRID_SIZE * GRID_SIZE;
 
   for (let i = 0; i < TOTAL_SQUARES; i++) {
@@ -38,9 +69,14 @@ button.addEventListener("click", () => {
     square.style.height = "30px";
     square.style.border = "1px solid #ccc"
     square.addEventListener("mouseover", () => {
+      if (!isDrawing) return;
       square.style.backgroundColor = getRandomColor();
-    });
-
+      square.style.cursor = "crosshair"
+    })
     container.appendChild(square);
   }
+  
 });
+
+
+
